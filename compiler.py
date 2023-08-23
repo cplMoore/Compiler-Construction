423d383310da480f1e3c41eca71dc8faa8596451
Jacob Moore <103230326+cplMoore@users.noreply.github.com>
2023-08-23 16:31:21 -0500

    Add files via upload

---
+++ compiler.py
@@ -0,0 +1,6 @@
+# COMP 5210 Compiler Construction project.
+# Author: Jacob Moore jwm0083.
+# Author: Ben Hulsey bph0022.
+
+
+# Recognize tokens print out tokens and put them in a tokenizer.py file. 

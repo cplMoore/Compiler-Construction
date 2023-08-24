b3fdb36a3cc375f6385c3eaf5d5ffa47022fd9b3
Jacob Moore <jacobmoore93@gmail.com>
2023-08-23 20:42:27 -0500

    Added RE library.

--- compiler.py
+++ compiler.py
@@ -2,5 +2,7 @@
 # Author: Jacob Moore jwm0083.
 # Author: Ben Hulsey bph0022.
 
+import re
 
-# Recognize tokens print out tokens and put them in a tokenizer.py file. 
+# Recognize tokens print out tokens and put them in a tokenizer.py file.
+

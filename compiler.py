42b5dca44355d8d703daa57caa97cd18f5e6f9f0
Jacob Moore <jacobmoore93@gmail.com>
2023-08-25 10:11:58 -0500

    Took inspiration from the python docs page.

--- compiler.py
+++ compiler.py
@@ -2,7 +2,15 @@
 # Author: Jacob Moore jwm0083.
 # Author: Ben Hulsey bph0022.
 
+from typing import NamedTuple
 import re
 
 # Recognize tokens print out tokens and put them in a tokenizer.py file.
+# Class and tokenize method was used from https://docs.python.org/3/library/re.html?highlight=regular%20expression.
+class Token(NamedTuple):
+    type: str
+    value: str
+    line: int
+    column: int
 
+def tokenize(code):

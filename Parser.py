00b294b1736f7c6ef444610a989bab86ff064357
Benjamin Hulsey <121725178+TardisMoon@users.noreply.github.com>
2023-09-27 13:11:22 -0500

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -7,67 +7,65 @@
 # https://docs.python.org/3/library/symtable.html#module-symtable
 # for symbol table
 
-from sly import Lexer, Parser
-from tokenizer import Tokenizer
+from sly import Parser # import SLY
+import tokenizer #import tokenizer file
 
-# Error handling for unknown characters
-def ERROR(token):
-   print(f"Unknown character: {token.value[0]}")
-   token.lexer.skip(1)
-
-# Define the lexer
-lexer = Tokenizer()
-
-# Grammar rules
 class MyParser(Parser):
-    tokens = lexer.tokens
+    tokens = tokenizer.Tokenizer.tokens
 
-    # Define the precedence and associativity of operators
-    precedence = (
-        ('left', 'PLUS', 'MINUS'),
-        ('left', 'TIMES', 'DIVIDE'),
-    )
+    def __init__(self):
+        self.names = {}  # Symbol table
 
-    @_('Expr PLUS Term', 'Expr MINUS Term', 'Term')
+    @_('Expr')
+    def statement(self, p):
+        return p.Expr
+
+    @_('Expr PLUS Term')
     def Expr(self, p):
-        if len(p) == 1:
-            p[0] = p[1]
-        else:
-            if p[2] == '+':
-                p[0] = p[1] + p[3]
-            elif p[2] == '-':
-                p[0] = p[1] - p[3]
+        return ('+', p.Expr, p.Term)
 
-    @_('Term TIMES Factor', 'Term DIVIDE Factor', 'Factor')
+    @_('Expr MINUS Term')
+    def Expr(self, p):
+        return ('-', p.Expr, p.Term)
+
+    @_('Term')
+    def Expr(self, p):
+        return p.Term
+
+    @_('Term TIMES Factor')
     def Term(self, p):
-        if len(p) == 1:
-            p[0] = p[1]
-        else:
-            if p[2] == '*':
-                p[0] = p[1] * p[3]
-            elif p[2] == '/':
-                p[0] = p[1] / p[3]
+        return ('*', p.Term, p.Factor)
 
-    @_('LPAREN Expr RPAREN', 'NUM', 'ID')
+    @_('Term DIVIDE Factor')
+    def Term(self, p):
+        return ('/', p.Term, p.Factor)
+
+    @_('Factor')
+    def Term(self, p):
+        return p.Factor
+
+    @_('LPAREN Expr RPAREN')
     def Factor(self, p):
-        if len(p) == 4 and p[1] == '(' and p[2] == ')' and p[3] is None:
-            raise SyntaxError("Syntax error: Empty parentheses")
-        elif len(p) == 2:
-            p[0] = p[1]
+        return p.Expr
+
+    @_('NUM')
+    def Factor(self, p):
+        return int(p.NUM)
+
+    @_('ID')
+    def Factor(self, p):
+        return p.ID
 
     def error(self, p):
         raise SyntaxError(f"Syntax error at token {p.type}")
 
-# Build the parser
-parsers = MyParser()
+if __name__ == '__main__':
+    data = "1 + 2 * (3 - 4)"
+    lexer = tokenizer.Tokenizer()
+    parser = MyParser()
 
-# Creates output
-data = "1 + 2 * (3 - 4)"
-lexer = Tokenizer()
-tokens = []
-for tok in lexer.tokenize(data):
-    tokens.append((tok.type, tok.value))
-    print(tok)
-
-
-
+    try:
+        result = parser.parse(lexer.tokenize(data))
+        print(result)
+    except SyntaxError as e:
+        print(e)

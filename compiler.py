22fda638b3938022d1160124bc86495c306f3e20
Jacob Moore <jacobmoore93@gmail.com>
2023-10-31 21:52:22 -0500

    commit to pull.

--- compiler.py
+++ compiler.py
@@ -1,16 +1,17 @@
-#Compiler with different flag options for output
-#Author Ben Hulsey
-#Author Jacob Moore
-
-import parser 
+# Compiler with different flag options for output
+# Author Ben Hulsey
+# Author Jacob Moore
+ 
 import argparse
 import subprocess
 
-
+# JM: had a hard time with trying to call the programs with argparse. 
+# ChatGPT recommended using the subprocess module to run the programs.
 if __name__ == "__main__":
 
 
-    parser = argparse.ArgumentParser(description='Comand line interface for compiler.py. Intakes .c files.')
+    parser = argparse.ArgumentParser(description='Command line interface for compiler.py. Intakes .c files.',
+                                     epilog='War Eagle!')
     
     # Positional Arguments.
     # Must intake a .c file.
@@ -20,13 +21,13 @@ if __name__ == "__main__":
     # Flags for command line interface and what they will do.
     parser.add_argument('-t', '--tokenizer', action='store_true', help='tokenizer program')
     parser.add_argument('-p', '--parser', action='store_true', help='parser program')
-    
+    parser.add_argument('-o', '--optimize', action='store_true', help='optimizer program')
     args = parser.parse_args()
     
     if args.tokenizer:
         #Call tokenizer.py and outputs tokens.
-        subprocess.run(['python3', 'tokenizer.py', args.input_file], check=True)
+        subprocess.run(['python3', 'tokenizer.py', args.input_file], check=True, capture_output=False)
     
     elif args.parser:
         # Call parser.py
-        subprocess.run(['python3', 'parser.py', args.input_file], check=True)
+        subprocess.run(['python3', 'parser.py', args.input_file], check=True, capture_output=False)
